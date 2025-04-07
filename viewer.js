4c93e6c020038097af528d2f5ad75d4702b61f48
berkderis <derisberk@gmail.com>
2025-04-08 01:20:42 +0300

    Create viewer.js

---
+++ viewer.js
@@ -0,0 +1,10 @@
+(async function() {
+  try {
+    const res = await fetch("https://counter.fuzem-com.workers.dev/");
+    const data = await res.json();
+    const el = document.getElementById("viewer-count");
+    if (el && data.count) el.textContent = data.count;
+  } catch (e) {
+    console.error("Sayaç alınamadı", e);
+  }
+})();

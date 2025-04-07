f65bf24e1db9589176ff6d39c673152e0ff7e732
berkderis <derisberk@gmail.com>
2025-04-08 02:22:31 +0300

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -1,11 +1,16 @@
 (async function() {
   try {
-    const res = await fetch("https://counter.fuzem-com.workers.dev/", {
+    // Mevcut sayfanın yolunu alıyoruz
+    const currentPath = window.location.pathname;
+    
+    const res = await fetch(`https://viewer-counter.fuzem.workers.dev${currentPath}`, {
       mode: "cors"
     });
     const data = await res.json();
     const el = document.getElementById("viewer-count");
-    if (el && data.count) el.textContent = data.count;
+    if (el && data.count) {
+      el.textContent = new Intl.NumberFormat('tr-TR').format(data.count);
+    }
   } catch (e) {
     console.error("Sayaç alınamadı", e);
   }
